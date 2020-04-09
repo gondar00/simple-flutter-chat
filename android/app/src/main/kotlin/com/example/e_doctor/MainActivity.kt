5c4551d5a23d3b622a05307c1ae1e5c6ad6224ea
gandharv <gandharvkumargarg@gmail.com>
2020-04-10 03:49:21 +0530

    Revert "add platform specific opentok sdk which is trigged through flutter platform channel"
    This reverts commit 9d3c30922b0b739a682da019afd05617ad330448.

--- android/app/src/main/kotlin/com/example/e_doctor/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/e_doctor/MainActivity.kt
@@ -1,29 +1,12 @@
-package com.example.e_doctor
+package com.project.e_doctor
 
 import androidx.annotation.NonNull;
-import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
-    // private val CHANNEL = "com.video.sdk/opentok"
-
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
-
-        // MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-        //     // Note: this method is invoked on the main thread.
-        //     call, result ->
-        //     if (call.method == "openVideoChat") {
-        //         val intent = Intent(this, VideoActivity::class.java)
-        //         startActivity(intent)
-        //         // result.error("UNAVAILABLE", "Battery level not available.", null)
-
-        //     } else {
-        //         result.notImplemented()
-        //     }
-        // }
     }
 }
